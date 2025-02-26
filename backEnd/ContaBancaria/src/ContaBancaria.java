c6506fb3626918a5d32c99b275404a971d55a42b
DaniloSantos284 <daniloribeiro761@gmail.com>
2025-02-26 16:25:34 -0300

    commitando a base java na principal de exemplo, mas ela será implementada na branche teste e depois darei o merge para a main

---
+++ backEnd/ContaBancaria/src/ContaBancaria.java
@@ -0,0 +1,20 @@
+public class ContaBancaria {
+    
+    //Atributos da classe ContaBancaria
+    String titular;
+    String numeroDaConta;
+    Double saldo;
+
+    // Metodos da classe
+    public void exibirSaldo() {
+
+    }
+
+        public void transferir() {
+
+        }
+
+            public void sacar() {
+                
+            }
+}
